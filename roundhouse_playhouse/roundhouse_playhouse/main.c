b17f8218a234f0c905c60e9161bf7255f0e2e9a5
MarekBillington <marek.billington@gmail.com>
2015-09-27 21:34:56 +1300

    Introduction of Opponent

--- roundhouse_playhouse/roundhouse_playhouse/main.c
+++ roundhouse_playhouse/roundhouse_playhouse/main.c
@@ -6,6 +6,7 @@
 #include "assets\background.h"
 #include "assets\titlescreen.h"
 #include "assets\player_still.h"
+#include "assets\opponent_still.h"
 //include headers that contain the sprites
 
 //memory location definers
@@ -37,17 +38,17 @@ void PlotPixelTransparent(int x, int y, unsigned short int c)
 {
 	//set video buffer to draw sprites and remove white space
 	unsigned short int temp;
-	if ((c & 0x00FF) == 0)
+	if ((c & 0xFF00) == 0) // bottom is transparent
 	{
-		if ((c & 0xFF00) == 0)
-		{
+		if ((c & 0x00FF) == 0) // top is also transparent
 			return;
-		}
+		// so bottom transparent, top is not so....
 		temp = ((videoBuffer[(y)* 120 + (x)]) & 0x00FF);
-		temp |= c;
+		temp |= c; // or it with c (to get the highpart of c in
 		videoBuffer[(y)* 120 + (x)] = (temp);
 	}
-	else if ((c & 0xFF00) == 0)
+	else
+	if ((c & 0xFF00) == 0) // only the top is transprent
 	{
 		temp = ((videoBuffer[(y)* 120 + (x)]) & 0xFF00);
 		temp |= c;
@@ -55,8 +56,162 @@ void PlotPixelTransparent(int x, int y, unsigned short int c)
 	}
 	else
 	{
-		videoBuffer[(y)* 120 + (x)];
+		videoBuffer[(y)* 120 + (x)] = (c);
 	}
+}
+
+//declare functions
+void WaitForStart(void);
+void Flip(void);
+void WaitForVblank(void);
+void EraseScreen(void);
+void DoIntro(void);
+void DrawBackground(void);
+void ChangeBackPal(void);
+void DrawPlayerLife(int pLife);
+void DrawOpponentsLife(int oLife);
+void DrawPlayer(int xplayer, int yplayer, int direction);
+void DrawOpponent(int x, int y, int direciton);
+void DrawTime(int time);
+void DrawLost(void);
+void DrawWin(void);
+
+//main function that controls the game loop
+int main(void)
+{
+	SetMode(MODE_4 | BG2_ENABLE);
+
+	EraseScreen();
+	DoIntro();
+
+	while (1)
+	{
+		//stays in this loop until exiting game
+		EraseScreen();
+		//everything is setup at the start of the loop until going into the next loop which controls manages the game in play mode
+		int pl_life; //life is 1 to 100
+
+		int op_life; //life is 1 to 200
+
+		int playx, playy; //x and y position of the player
+
+		int oppx, oppy; //x an y position of the opponent
+
+		int rnum;
+
+		//controls what state the player and opponent
+		enum FighterStates pl_state = still;
+		enum FighterStates opp_state = still;
+
+		playx = 60;
+		playy = 155 - player_still_HEIGHT;
+		oppx = 30;
+		oppy = 130 - opponent_still_HEIGHT;
+		pl_life = 0;
+		op_life = 0;
+
+		//ChangeBackPal();
+
+		while (pl_life < 100)
+		{
+
+			DrawBackground();
+			DrawPlayerLife(pl_life);
+			DrawOpponentsLife(op_life);
+			op_life++;
+			pl_life++;
+
+			WaitForVblank();
+			Flip();
+		}
+
+		while ((pl_state != dead) || (opp_state != dead))
+		{
+			// prolly want to do a screen flip here for speed????
+			// anyhow method is as follows
+			// 1. draw the bg
+			// 2. draw the bombs, life, tank
+			// 3. flip it to the screen
+			// check for user input
+			// move the bombs
+			// check for collision
+			// subtract from life
+
+			DrawBackground();
+			DrawPlayerLife(pl_life);
+			DrawOpponentsLife(op_life);
+			//DrawTime(time);
+			DrawOpponent(oppx, oppy, 0);
+			DrawPlayer(playx, playy, 0);
+			WaitForVblank();
+			Flip();
+
+			//player movement and subsequent opponent response
+
+			if (!((*KEYS) & KEY_RIGHT))
+			{
+				playx += 2;
+			}
+			if (!((*KEYS) & KEY_LEFT))
+			{
+				playx -= 2;
+			}
+			
+			//boundarycheck for player and opponent
+
+			if (playx < 0)
+			{
+				playx = 0;
+			}
+			if (playx > (240 - player_still_WIDTH) / 2)
+			{
+				playx = (240 - player_still_WIDTH) / 2;
+			}
+
+			//control opponent
+
+			if (playx < oppx)
+			{
+
+				if (oppx < 0)
+				{
+					oppx = 0;
+				}
+				else if (oppx >(240 - opponent_still_WIDTH) / 2)
+				{
+					oppx = (240 - opponent_still_WIDTH) / 2;
+				}
+				else 
+				{
+					oppx -= 2;
+				}
+			}
+			if (playx > oppx) {
+
+				if (oppx < 0)
+				{
+					oppx = 0;
+				}
+				else if (oppx >(240 - opponent_still_WIDTH) / 2)
+				{
+					oppx = (240 - opponent_still_WIDTH) / 2;
+				}
+				else
+				{
+					oppx += 2;
+				}
+			}
+
+			//control deltatime
+		}
+
+		
+		WaitForVblank();
+		Flip();
+		//draw, win or loss depending game result
+
+	}
+
 }
 
 void WaitForStart(void)
@@ -183,20 +338,29 @@ void DrawPlayer(int xplayer, int yplayer, int direction)
 {
 	//draws the player
 	int x, y;
-	
+
 	for (y = 0; y < player_still_HEIGHT; y++)
 	{
-		for (x = 0; x < (player_still_WIDTH/2); x++)
+		for (x = 0; x < (player_still_WIDTH / 2); x++)
 		{
-			PlotPixel(x+xplayer, y+yplayer, player_stillData[y * (player_still_WIDTH / 2) + x]);
+			PlotPixel(x + xplayer, y + yplayer, player_stillData[y * (player_still_WIDTH / 2) + x]);
 		}
 	}
-	
+
 }
 
-void DrawOpponent(int x, int y, int direciton)
+void DrawOpponent(int xopp, int yopp, int direciton)
 {
 	//draws the opponent
+	int x, y;
+
+	for (y = 0; y < opponent_still_HEIGHT; y++)
+	{
+		for (x = 0; x < (opponent_still_WIDTH / 2); x++)
+		{
+			PlotPixel(x + xopp, y + yopp, opponent_stillData[y * (opponent_still_WIDTH / 2) + x]);
+		}
+	}
 }
 
 void DrawTime(int time)
@@ -212,107 +376,5 @@ void DrawLost(void)
 void DrawWin(void)
 {
 	//draws the screen when the player wins
-}
-
-
-
-//main function that controls the game loop
-int main(void)
-{
-	SetMode(MODE_4 | BG2_ENABLE);
-
-	EraseScreen();
-	DoIntro();
-
-	while (1)
-	{
-		//stays in this loop until exiting game
-		EraseScreen();
-		//everything is setup at the start of the loop until going into the next loop which controls manages the game in play mode
-		int pl_life; //life is 1 to 100
-
-		int op_life; //life is 1 to 200
-
-		int playx, playy; //x and y position of the player
-
-		int oppx, oppy; //x an y position of the opponent
-
-		int time;
-
-		//controls what state the player and opponent
-		enum FighterStates pl_state = still;
-		enum FighterStates opp_state = still;
-
-		playx = 60;
-		playy = 155 - player_still_HEIGHT;
-		pl_life = 0;
-		op_life = 0;
-		time = 60;
-
-		ChangeBackPal();
-
-		while (pl_life < 100)
-		{
-
-			DrawBackground();
-			DrawPlayerLife(pl_life);
-			DrawOpponentsLife(op_life);
-			op_life++;
-			pl_life++;
-
-			WaitForVblank();
-			Flip();
-		}
-
-		while ((pl_state != dead) || (opp_state != dead))
-		{
-			// prolly want to do a screen flip here for speed????
-			// anyhow method is as follows
-			// 1. draw the bg
-			// 2. draw the bombs, life, tank
-			// 3. flip it to the screen
-			// check for user input
-			// move the bombs
-			// check for collision
-			// subtract from life
-
-			DrawBackground();
-			DrawPlayerLife(pl_life);
-			DrawOpponentsLife(op_life);
-			//DrawTime(time);
-			//DrawOpponent(oppx, oppy, 0);
-			DrawPlayer(playx, playy, 0);
-			WaitForVblank();
-			Flip();
-
-			//player movement and subsequent opponent response
-
-			if (!((*KEYS) & KEY_RIGHT))
-			{
-				playx += 2;
-			}
-			if (!((*KEYS) & KEY_LEFT))
-			{
-				playx -= 2;
-			}
-			
-			//boundarycheck for player and opponent
-
-			if (playx < 0)
-			{
-				playx = 0;
-			}
-			//if (playx > ())
-
-			//control deltatime
-		}
-
-		
-		WaitForVblank();
-		Flip();
-		//draw, win or loss depending game result
-
-	}
-
 }
 
